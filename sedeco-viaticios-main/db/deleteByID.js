81cf778d2f46b89264c405efdfed5a88319e7bd1
Jorge Martinez <102623373+Jorge058@users.noreply.github.com>
2025-07-08 16:57:58 -0600

    Direcciones agregadas

--- sedeco-viaticios-main/db/deleteByID.js
+++ sedeco-viaticios-main/db/deleteByID.js
@@ -17,12 +17,12 @@ const db = getFirestore(app);
 console.log("Firebase Firestore inicializado correctamente.");
 
 // Selecciona todos los botones de eliminar
-/* document.addEventListener("click", function (e) {
+document.addEventListener("click", function (e) {
   if (e.target.closest(".BtnBorrar")) {
     const documentId = e.target.closest(".BtnBorrar").id;
     deleteViaticos(documentId);
   }
-}); */
+});
 
 // Corre sweet alert de confirmación
 function deleteViaticos(id) {
